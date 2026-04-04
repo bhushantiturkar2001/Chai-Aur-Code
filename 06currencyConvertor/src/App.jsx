8a55557d84c7d1d25b24b22cf8862b7cdb234b70
bhushantiturkar2001 <titurkarbhushan@gmail.com>
2026-04-04 19:40:08 +0530

    Remove old api for currency

--- 06currencyConvertor/src/App.jsx
+++ 06currencyConvertor/src/App.jsx
@@ -44,7 +44,7 @@ function App() {
                 label="From"
                 amount={amount}
                 currencyOptions={options}            
-                onCurrencyChange={(currency)=> setAmount(currency)}
+                onCurrencyChange={(currency)=> setFrom(currency)}
                 selectCurrency={from}
                 onAmountChnage={(amount)=>{
                 setAmount(amount)
