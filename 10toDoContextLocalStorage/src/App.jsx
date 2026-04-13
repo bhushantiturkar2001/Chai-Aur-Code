0ca2d0d43e26092631a878bad66590c074d76776
bhushantiturkar2001 <titurkarbhushan@gmail.com>
2026-04-13 23:34:02 +0530

    Mthod add, delte

--- 10toDoContextLocalStorage/src/App.jsx
+++ 10toDoContextLocalStorage/src/App.jsx
@@ -1,21 +1,49 @@
-import { useState } from "react";
+import { use, useEffect, useState } from "react";
 import "./App.css";
 import { ToDoProvider } from "./context";
 
 function App() {
+  const [todos, setTodos] = useState([]);
 
-const [todos, setTodos] = useState([])
+  const addToDo = (todos) => {
+    setTodos((prev) => [{ id: Date.now, ...todos }, ...prev]);
+  };
 
-const addToDo = (todos) => {
-   setTodos((prev) => [{id: Date.now, ...todos},...prev])
-}
+  const updateToDo = (id, todo) => {
+    setTodos((prev) =>
+      prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo))
+    );
+  };
 
+  const deleteToDo = (id) => {
+    setTodos((prev) => prev.filter((todo) => todo.id !== id));
+  };
 
+  const toggleComplete = (id) => {
+    setTodos((prev) =>
+      prev.map((prevTodo) =>
+        prevTodo === id
+          ? { ...prevTodo, completed: !prevTodo.completed }
+          : prevTodo
+      )
+    );
+  };
 
+  useEffect(() => {
+    JSON.parse(localStorage.getItem("todos"));
+    if (todos && todos.length > 0) {
+      setTodos(todos);
+    }
+  }, []);
 
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
   return (
-    <ToDoProvider value={{todos, addToDo, updateToDo, deleteToDo, toggleComplete}}>
+    <ToDoProvider
+      value={{ todos, addToDo, updateToDo, deleteToDo, toggleComplete }}
+    >
       <div className="bg-[#172842] min-h-screen py-8">
         <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">
