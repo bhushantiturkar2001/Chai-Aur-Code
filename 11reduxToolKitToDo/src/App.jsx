d23320029bfb92a6bebe3de16068d25c5789cf4c
bhushantiturkar2001 <titurkarbhushan@gmail.com>
2026-04-16 22:50:46 +0530

    PRoject set up and code

---
+++ 11reduxToolKitToDo/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import AddToDo from './components/AddToDo'
+import Todos  from './components/Todos'
+
+function App() {
+  
+  return (
+    <>
+     <h1>Learn Redux</h1>
+     <AddToDo/>
+     <Todos/>     
+    </>
+  )
+}
+
+export default App
