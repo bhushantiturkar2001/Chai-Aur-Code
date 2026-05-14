da31bd4928740cf3cc0559a1cdd38c58b1e6ca3c
bhushantiturkar2001 <titurkarbhushan@gmail.com>
2026-05-14 15:17:45 +0530

    Solve bugs of form

--- 12MegaBlogSite/src/component/Header/LogoutBtn.jsx
+++ 12MegaBlogSite/src/component/Header/LogoutBtn.jsx
@@ -1,20 +1,24 @@
 import React from 'react'
 import { useDispatch } from 'react-redux'
-import { logout } from '../../api/auth'
-
-
+import authService from '../../appwrite/auth'
+import { logout } from '../../store/authSlice'
 
 function LogoutBtn() {
 
   const dispatch = useDispatch()
-  const logoutHandler = ()=>{
-    authService.logout().then(()=>{
-        dispatch(logout())
+  const logoutHandler = () => {
+    authService.logout().then(() => {
+      dispatch(logout())
     })
   }
 
   return (
-    <div>LogoutBtn</div>
+    <button
+      className='inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
+      onClick={logoutHandler}
+    >
+      Logout
+    </button>
   )
 }
 
--- 12MegaBlogSite/src/main.jsx
+++ 12MegaBlogSite/src/main.jsx
@@ -5,14 +5,15 @@ import './index.css'
 import { Provider } from 'react-redux'
 import store from './store/store.js'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import Home from './pages/Home.jsx'
-import { AuthLayout, Login } from './components/index.js'
+import { AuthLayout } from './components/index.js'
 
-import AddPost from "./pages/AddPost";
-import Signup from './pages/Signup'
-import EditPost from "./pages/EditPost";
-import Post from "./pages/Post";
-import AllPosts from "./pages/AllPosts";
+import Home from './pages/Home.jsx'
+import Login from './pages/Login.jsx'
+import Signup from './pages/Signup.jsx'
+import AddPost from './pages/AddPost.jsx'
+import EditPost from './pages/EditPost.jsx'
+import Post from './pages/Post.jsx'
+import AllPosts from './pages/AllPosts.jsx'
 
 const router = createBrowserRouter([
   {
